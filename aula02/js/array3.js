0fb925893486516bfeb4cf27f9682f04d044ee9a
William Santana <71521910+wbsfps@users.noreply.github.com>
2024-08-12 20:51:49 -0300

    Add files via upload

---
+++ aula02/js/array3.js
@@ -0,0 +1,22 @@
+var daysOfWeek = new Array();
+// var daysOfWeek = new Array(7); // aplicar tamanho do array
+
+daysOfWeek[0] = 'Domingo';
+console.log(daysOfWeek.length);
+
+daysOfWeek[0] = 'Domingo';
+console.log(daysOfWeek.length);
+
+var daysOfWeek = new Array();
+
+var daysOfWeek = new Array(
+    'Domingo',
+    'Segunda',
+    'Terça',
+    'Quarta',
+    'Quinta',
+    'Sexta',
+    'Sábado'
+);
+console.log(daysOfWeek.length);
+console.log(daysOfWeek[3]);
---
+++ aula02/js/array4.js
@@ -0,0 +1,3 @@
+let daysOfWeek = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+
+for (let i = 0; i < daysOfWeek.length; i++)  console.log(daysOfWeek[i]);
