99e16dea00721366731f8e2656fe9af9d7d33837
William Santana <williamandrade1058@gmail.com>
2024-09-01 14:25:15 -0300

    estrutura de dados fila

---
+++ fila/externals/main.js
@@ -0,0 +1,44 @@
+function Queue () {
+    let items = [];
+    
+    this.peek = () => {
+        return items[0];
+    };
+
+    this.enqueue = (element) => {
+        return items.push(element);
+    }
+
+    this.dequeue = () => {
+        return items.shift();
+    }
+
+    this.print = () => {
+        console.log(items);
+    }
+
+    this.isEmpty = () => {
+        return items.length === 0;
+    }
+
+    this.size = () => {
+        return items.length;
+    }
+}
+
+let queue = new Queue();
+
+queue.enqueue("Vasco");
+queue.enqueue("Fluminense");
+queue.enqueue("Flamengo");
+
+let removed = queue.dequeue();
+console.log(`Item removed was ${removed}`);
+
+queue.print()
+
+console.log(queue.peek());
+
+console.log(queue.isEmpty());
+
+console.log("Size: " + queue.size());
